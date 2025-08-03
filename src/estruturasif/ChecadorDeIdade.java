1db2d093de6039630966cde46156dbaf9f9fb373
alinor <alinor@zorin.os>
2025-08-03 14:30:02 -0300

    Estrutura If (Checador de Idade

---
+++ src/estruturasif/ChecadorDeIdade.java
@@ -0,0 +1,20 @@
+package estruturasif;
+
+import java.util.Scanner;
+
+public class ChecadorDeIdade {
+    public static void main(String[] args) {
+        System.out.println("Checador de idade");
+        System.out.println("Digite sua idade: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int idade = scanner.nextInt();
+
+        if (idade >= 18) {
+            System.out.println("Pode comprar o ingresso");
+        } else {
+            System.out.println("Esse filme é para maiores de 18 anos.");
+        }
+    }
+}
